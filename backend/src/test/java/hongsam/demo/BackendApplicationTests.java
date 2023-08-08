d8eb2043dbb5e65308b73f25c2e0a40eb22543ad
seoyeoning <minalunpark@naver.com>
2023-08-08 20:45:47 +0900

    setting

---
+++ backend/src/test/java/hongsam/demo/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hongsam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
