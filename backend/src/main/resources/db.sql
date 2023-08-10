ff8c326fe45cd0eb1a683e98efbdeff0d48334ac
dbstn1325 <dbstn1325@naver.com>
2023-08-10 11:48:18 +0900

    docs: db.sql 추가

---
+++ backend/src/main/resources/db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    displayName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE board (
+    boardId BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    memberId BIGINT,
+    FOREIGN KEY (memberId) REFERENCES member(id)
+);
