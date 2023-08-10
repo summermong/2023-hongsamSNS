a0aa32c7079bc0e4e14beaeef891efa9df2d0c99
강경규 <qwzx16@naver.com>
2023-08-10 20:59:36 +0900

    feature: App.js update yarn install index.js update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import './App.css';
-import Login from './pages/Login';
-import Join from './pages/Join';
-import Home from './pages/Home';
-import axios from 'axios';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import Main from "./pages/Main";
+import CreatePage from "./pages/CreatePage";
+import Serch from "./pages/Serch";
+import Profile from "./pages/Profile";
+import UpdatePage from "./pages/UpdatePage";
+import Login from "./pages/Login";
+import Join from "./pages/Join";
+import axios from "axios";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
-
+  const [isLogin, setIsLogin] = useState(true);
+  const navigator = useNavigate();
   useEffect(() => {
     axios
       .get(
-        'https://6e32-2406-5900-103c-d815-f572-5dff-7e00-d937.ngrok-free.app/home',
+        "https://6e32-2406-5900-103c-d815-f572-5dff-7e00-d937.ngrok-free.app/home",
         { withCredentials: true }
       )
       .then((response) => {
@@ -21,23 +26,139 @@ function App() {
         setIsLogin(data.isLogin);
       })
       .catch((error) => {
-        console.error('Error checking session:', error);
+        console.error("Error checking session:", error);
       });
   }, []);
 
+  const [items, setItems] = useState([]);
+
+  const fecthItems = async () => {
+    await axios
+      .get("http://localhost:4000/item")
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fecthItems();
+  }, []);
+
+  const createItem = async (title, content, url) => {
+    await axios
+      .post("http://localhost:4000/item", {
+        boardId: 0,
+        title: title,
+        content: content,
+        memberId: 0,
+        displayName: "또띠",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("요청실패");
+        console.log(err);
+        console.log(title, content);
+      });
+    navigator(url);
+    fecthItems();
+  };
+  const deleteItem = (id) => {
+    axios
+      .get("http://localhost:4000/item")
+      .then((res) => {
+        console.log("delete : ", res.data);
+        console.log("성공");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios.delete(`http://localhost:4000/item/${id}`);
+    fecthItems();
+  };
+  const updateItem = async (title, content, id, url) => {
+    await axios.patch(`http://localhost:4000/item/${id}`, {
+      boardId: 0,
+      title: title,
+      content: content,
+      memberId: 0,
+      displayName: "또띠",
+    });
+    navigator(url);
+    fecthItems();
+  };
+  const updateText = (id) => {
+    let copyItems = [...items];
+    const itemIndex = copyItems.findIndex((el) => {
+      return el["id"] === +id;
+    });
+    return itemIndex;
+  };
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          {isLogin ? (
-            <Route path="/" element={<Home />} />
-          ) : (
-            <Route path="/" element={<Login setIsLogin={setIsLogin} />} />
-          )}
-          <Route path="/join" element={<Join />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Routes>
+        {isLogin ? (
+          <Route
+            path="/"
+            element={
+              <>
+                <Main
+                  items={items}
+                  setItems={setItems}
+                  deleteItem={deleteItem}
+                ></Main>
+              </>
+            }
+          ></Route>
+        ) : (
+          <Route path="/" element={<Login setIsLogin={setIsLogin} />} />
+        )}
+        <Route path="/join" element={<Join />} />
+
+        <Route
+          path="/create"
+          element={
+            <>
+              <CreatePage createItem={createItem}></CreatePage>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/serch"
+          element={
+            <>
+              <Serch></Serch>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/profile"
+          element={
+            <>
+              <Profile></Profile>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/update/:id"
+          element={
+            <>
+              <UpdatePage
+                items={items}
+                setItems={setItems}
+                updateItem={updateItem}
+                updateText={updateText}
+              />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </div>
   );
 }
 
