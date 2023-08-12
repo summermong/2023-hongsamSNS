d97283de8014312b9969f628d27a3f0691ece1d1
woo <ldw28517@naver.com>
2023-08-12 17:30:37 +0900

    feat: 비밀번호 Validation

--- backend/src/main/java/hongsam/demo/member/domain/MemberDto.java
+++ backend/src/main/java/hongsam/demo/member/domain/MemberDto.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -19,10 +20,12 @@ public class MemberDto {
     private String displayName;
 
     @NotEmpty
-    @Email
+    @Email(message = "올바르지 않는 이메일 형식입니다.")
     private String email;
 
     @NotEmpty
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
     public MemberDto() {
