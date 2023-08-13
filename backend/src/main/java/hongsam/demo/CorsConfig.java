571bea01e25d937e25973bda60c30ea018cae158
woo <ldw28517@naver.com>
2023-08-13 12:03:01 +0900

    feat: 백엔드 요청 URL 수정

--- backend/src/main/java/hongsam/demo/CorsConfig.java
+++ backend/src/main/java/hongsam/demo/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해 CORS 설정 적용
-                .allowedOrigins("https://a831-13-209-119-114.ngrok-free.app", "http:localhost:3000") // 허용할 도메인 지정
+                .allowedOrigins("https://d138-13-209-119-114.ngrok-free.app", "http:localhost:3000", "https://318b-1-217-43-45.ngrok-free.app") // 허용할 도메인 지정
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS") // 허용할 HTTP 메서드 지정
                 .allowedHeaders("Content-Type", "Cookie", "headers", "*") // 허용할 헤더 지정
                 .allowCredentials(true) // 인증 정보 허용
